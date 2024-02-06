f7fc191bcfb3c5101b7e98de817fecb60fef260c
Никита <nik.glazkov2016@yandex.ru>
2024-02-06 16:33:48 +0300

    Добавьте файлы проекта.

---
+++ OTUS_DZ6_DELEGATES_EVENTS/Foo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTUS_DZ6_DELEGATES_EVENTS
+{
+    public static class Foo
+    {
+        public static T GetMax<T>(this IEnumerable<T> collection, Func<T, float> convertToNumber) where T : class
+        {
+
+        }
+    }
+}
---
+++ OTUS_DZ6_DELEGATES_EVENTS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OTUS_DZ6_DELEGATES_EVENTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
